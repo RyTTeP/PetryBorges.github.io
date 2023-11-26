ce1608fee171925463adba7d0d45217ef5bca4b8
Rytep <128534510+RyTTeP@users.noreply.github.com>
2023-11-26 19:25:58 -0300

    Add files via upload

---
+++ SiteTRAB/script.js
@@ -0,0 +1,140 @@
+function Icon() {
+  window.location.href = "index.html"
+}
+
+let icon = document.getElementById("icon")
+icon.onclick = function() {
+  Icon();
+}
+    var back = document.getElementById("Back");
+    var opcoesback = document.getElementById("opcoesBack");
+      back.addEventListener('mouseover', function() {
+        opcoesback.style.display = "block";
+      });
+  
+      back.addEventListener('mouseout', function() {
+        opcoesback.style.display = "none";  
+      });
+      opcoesback.addEventListener('mouseover', function() {
+        opcoesback.style.display = "block";
+      });
+  
+      opcoesback.addEventListener('mouseout', function() {
+        opcoesback.style.display = "none";  
+      });
+      
+      function Java() {
+        window.location.href = "java.html"
+      }
+
+      let java = document.getElementById("Java")
+      java.onclick = function() {
+        Java();
+      }
+      
+      
+      function C() {
+         window.location.href = "C.html"
+      }
+
+      let CC = document.getElementById("C")
+      CC.onclick = function(){
+        C();
+      }
+
+      var front = document.getElementById("Front");
+    var opcoesfront = document.getElementById("opcoesFront");
+      front.addEventListener('mouseover', function() {
+        opcoesfront.style.display = "block";
+      });
+  
+      front.addEventListener('mouseout', function() {
+        opcoesfront.style.display = "none";  
+      });
+      opcoesfront.addEventListener('mouseover', function() {
+        opcoesfront.style.display = "block";
+      });
+  
+      opcoesfront.addEventListener('mouseout', function() {
+        opcoesfront.style.display = "none";  
+      });
+     
+     function HTML() {
+      window.location.href = "HTML.html"
+   }
+
+   let html = document.getElementById("HTML")
+   html.onclick = function(){
+     HTML();
+   }
+
+   function JS() {
+    window.location.href = "JavaScript.html"
+ }
+
+ let js = document.getElementById("JS")
+ js.onclick = function(){
+   JS();
+ }
+
+ var exatas = document.getElementById("Exatas");
+    var opcoesExatas = document.getElementById("opcoesExatas");
+      exatas.addEventListener('mouseover', function() {
+        opcoesExatas.style.display = "block";
+      });
+  
+      exatas.addEventListener('mouseout', function() {
+        opcoesExatas.style.display = "none";  
+      });
+      opcoesExatas.addEventListener('mouseover', function() {
+        opcoesExatas.style.display = "block";
+      });
+  
+      opcoesExatas.addEventListener('mouseout', function() {
+        opcoesExatas.style.display = "none";  
+      });
+      
+      function GAAL() {
+        window.location.href = "GAAL.html"
+     }
+    
+     let gaal = document.getElementById("GAAL")
+     gaal.onclick = function(){
+       GAAL();
+     }
+
+     function Estatistica() {
+      window.location.href = "Estatistica.html"
+   }
+  
+   let estatistica = document.getElementById("Estatistica")
+   estatistica.onclick = function(){
+     Estatistica();
+   }
+
+   var robotica = document.getElementById("Robotica");
+    var opcoesrobotica = document.getElementById("opcoesRobotica");
+      robotica.addEventListener('mouseover', function() {
+        opcoesrobotica.style.display = "block";
+      });
+  
+      robotica.addEventListener('mouseout', function() {
+        opcoesrobotica.style.display = "none";  
+      });
+      opcoesrobotica.addEventListener('mouseover', function() {
+        opcoesrobotica.style.display = "block";
+      });
+  
+      opcoesrobotica.addEventListener('mouseout', function() {
+        opcoesrobotica.style.display = "none";  
+      });
+
+      function Arduino() {
+        window.location.href = "Arduino.html"
+     }
+    
+     let arduino = document.getElementById("Arduino")
+     arduino.onclick = function(){
+       Arduino();
+       
+     }
